299433e871f795a1a476fc0da2797f6f85d2b8ab
vang_machine <vangogius@gmail.com>
2018-06-06 01:37:12 +0300

    Additional info to setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,26 @@ from setuptools import setup, find_packages
 
 
 requirements = [
-    "numpy>=1.14.3",
-    "PyStemmer==1.3.0",
-    "regex>=2018.2.21",
+    'numpy>=1.14.3',
+    'PyStemmer==1.3.0',
+    'regex>=2018.2.21',
 ]
 
 setup(
-    name="tldry",
-    version="0.1.0",
-    description="Automatic text summarizer using BM25 and textrank",
+    name='tldry',
+    version='0.1.0',
+    author='Artem Vang',
+    author_email='vangogius@gmail.com',
+    url='https://github.com/vangaa/tldry',
+    description='Automatic text summarizer using BM25 and textrank',
+    license='MIT License',
     install_requires=requirements,
     packages=find_packages('.', exclude='tests'),
+    classifiers=(
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: Implementation :: CPython',
+    )
 )
